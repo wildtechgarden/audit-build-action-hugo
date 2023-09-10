29f0f4864587e34dbd35fbad97a362b40582ae93
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-09-09 22:05:39 -0400

    Remove missed starter repo references (#2)

--- scripts/local_build_deploy.sh
+++ scripts/local_build_deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# cspell:ignore modstarter
+# cspell:ignore auditdefault
 
 set -e
 set -o pipefail
 
 rm -rf public
 
-export BASEURL="https://www.module-starter.wtg-demos.ca/"
+export BASEURL="https://www.audit-build-action-hugo-default.wtg-demos.ca/"
 export HUGO_RESOURCEDIR="$(pwd)"/resources
-export SITECONFIG="$(pwd)"/tests/config/hugo.toml
+export SITECONFIG="$(pwd)"/tests/config/hugo.toml,"$(pwd)"/tests/config/variations/config-default.toml,"$(pwd)"/tests/config/variations/config-latest.toml
 export TARGET="$(pwd)"/public
 export CURDIR="$(pwd)"
 
 cd tests/config && hugo --gc --minify -b $BASEURL --source "$(pwd)" --destination "${TARGET}" --config "${SITECONFIG}"
 cd "${CURDIR}"
-rclone sync --progress public/ wtg-modstarter:./
+rclone sync --progress public/ wtg-auditdefault:./
